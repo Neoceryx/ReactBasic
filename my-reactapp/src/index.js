92e3556320157ad1843fb2fd2ec5c941a1a5f0dc
daniel.fierro <daniel.fierro@mitechnologiesinc.com>
2020-03-11 14:21:38 -0700

    quedo pendiente mostrar la lista del pronostico del clima

--- my-reactapp/src/index.js
+++ my-reactapp/src/index.js
@@ -2,77 +2,77 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class SingleForm extends React.Component{
+class SingleForm extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {username:""};
+        this.state = { username: "" };
     }
 
-    handleChange = event=>{        
-        this.setState({username: event.target.value})
+    handleChange = event => {
+        this.setState({ username: event.target.value })
     }
 
-    render(){
+    render() {
 
-        return(
+        return (
             <React.Fragment>
-            <form>
-              <label>username</label>
-              <input
-                type="text"
-                name="username"
-                value={this.state.username}
-                onChange={this.handleChange}
-              />
-            </form>
-     
-            <h3>Your username is: {this.state.username}</h3>
-          </React.Fragment>
+                <form>
+                    <label>username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                </form>
+
+                <h3>Your username is: {this.state.username}</h3>
+            </React.Fragment>
         );
-        
+
     }
     // End Render Method
-    
+
 }
 // End Component
 
-class MultipleForm extends React.Component{
+class MultipleForm extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {username:"", password:""};
-    }    
-
-    // To Assign Dinamycly to the variable , the typed value according each field
-    handleChange = ({target}) =>{        
-        this.setState({[target.name]:target.value });
+        this.state = { username: "", password: "" };
     }
 
-    render(){
-        return(
+    // To Assign Dinamycly to the variable , the typed value according each field
+    handleChange = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+    }
+
+    render() {
+        return (
 
             <React.Fragment>
                 <form>
                     <label htmlFor="username">Username</label>
                     <input
-                    type="text"
-                    name="username"
-                    value={this.state.username}
-                    onChange ={this.handleChange}
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
                     />
 
                     <label htmlFor="password">Password</label>
                     <input
-                    type="text"
-                    name="password"
-                    value={this.state.password}
-                    onChange ={this.handleChange}
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
                     />
 
                 </form>
 
-                <b>UserName: {this.state.username} </b> <br/>
+                <b>UserName: {this.state.username} </b> <br />
                 <b>Password: {this.state.password}</b>
             </React.Fragment>
 
@@ -82,33 +82,33 @@ class MultipleForm extends React.Component{
 }
 // End Component
 
-class DisplayAlert extends React.Component{
+class DisplayAlert extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {msg:""}
+        this.state = { msg: "" }
     }
 
-    handleChange = event=>{        
-        this.setState({msg: event.target.value})
+    handleChange = event => {
+        this.setState({ msg: event.target.value })
     }
 
-    handleClick = event =>{
-        
+    handleClick = event => {
+
         // Avoid event propagation
-        event.preventDefault();        
+        event.preventDefault();
         var Message = this.state.msg;
         alert(Message);
-    } 
+    }
 
-    render(){
-        return(
+    render() {
+        return (
 
             <React.Fragment>
                 <form>
-                <label htmlFor="username">Username</label>
-                <textarea type="text" name="msg" value={this.state.msg} onChange={this.handleChange}>                    
-                </textarea>
+                    <label htmlFor="username">Username</label>
+                    <textarea type="text" name="msg" value={this.state.msg} onChange={this.handleChange}>
+                    </textarea>
                 </form>
 
                 <button type="button" onClick={this.handleClick}>Click Me</button>
@@ -120,56 +120,81 @@ class DisplayAlert extends React.Component{
 }
 // End Component
 
-class WeatherForecast extends React.Component{
+class WeatherForecast extends React.Component {
 
     // Start Contructor
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {weatherData:""};
+        this.state = { weatherData: "" };
     }
     // End constructor   
 
     // Document on ready
-    componentDidMount(){
+    componentDidMount() {
         console.log("Hello");
     }
     // End document on ready
 
-    GetWeatherForecast = event =>{
-        debugger
+    GetWeatherForecast = event => {
+        
         // validate if weather forecast is storaged in local
-        var IsforecastInLocal = localStorage.getItem("weather") != null ? localStorage.getItem("weather") : null ;
-        
-        // set the Url where the request would be sent
-        let URL ="https://api.weatherbit.io/v2.0/forecast/daily?city_id=3981609&days=16&key=19dbfc63db5446fbaeb3ea537b3bb17e";
-        
-        // Start http request
-        fetch(URL).then(async response=>{
+        var IsforecastInLocal = localStorage.getItem("weather") != null ? localStorage.getItem("weather") : null;
 
-            // gets api response 
-            var data = await response.json();
+        // Validate if weather forecast is in local
+        if (IsforecastInLocal == null) {
+
+            // set the Url where the request would be sent
+            let URL = "https://api.weatherbit.io/v2.0/forecast/daily?city_id=3981609&days=16&key=19dbfc63db5446fbaeb3ea537b3bb17e";
+
+            // Start http request
+            fetch(URL).then(async response => {
+
+                // gets api response 
+                var data = await response.json();
+
+                // storage the api response on localstorage. to avoid makes a request every time the page's reloading
+                localStorage.setItem("weather", JSON.stringify(data));
+
+                // Get the Weather forecast information
+                this.setState({ weatherData: data })
+
+            }).catch(error => { // Handling errors   
+                alert("An Error Ocurred while the weather indormation was getting");
+                console.log(error);
+            })
+            // End Http request
+
+        }else{
             
-            // storage the api response on localstorage. to avoid makes a request every time the page's reloading
-            localStorage.setItem("weather",JSON.stringify(data));
-
-        }).catch(error =>{ // Handling errors   
-            alert("An Error Ocurred while the weather indormation was getting");
-            console.log(error);
-        })
-        // End Http request
+            // Get the Weather forecast information from local
+            this.setState({ weatherData: JSON.parse(IsforecastInLocal) })
+            console.log(this.state.weatherData);
+        }  
+        // End forecast validation
 
     }
     // End function
 
-    render(){
+    render() {
 
         return (
             <React.Fragment>
-                <hr/>
+                <hr />
                 <div>
                     <button type="button" onClick={this.GetWeatherForecast}>
                         See Weather forecast
                     </button>
+
+                    <h3>Weather forectas from: {this.state.weatherData.city_name}</h3>
+
+                    <p>
+                        {JSON.stringify(this.state.weatherData.data)}
+                    </p>
+
+                    <div>
+                        
+                    </div>
+
                 </div>
             </React.Fragment>
         );
@@ -182,7 +207,7 @@ class WeatherForecast extends React.Component{
 
 ReactDOM.render(
     // To Display All the Components
-    [<SingleForm/>, <MultipleForm/>,<DisplayAlert/>,<WeatherForecast/>],
+    [<SingleForm />, <MultipleForm />, <DisplayAlert />, <WeatherForecast />],
 
     // To display Jus one Component
     //<DisplayAlert/>,
